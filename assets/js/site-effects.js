cd5c4300f2a8b05bb47621167a7d71c99f00e5cd
TBC-tec <hcharania@hotmail.com>
2025-08-23 13:22:22 -0500

    Create site-effects.js

---
+++ assets/js/site-effects.js
@@ -0,0 +1,8 @@
+// Smooth Scroll for all anchor links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href'))
+      .scrollIntoView({ behavior: 'smooth' });
+  });
+});
